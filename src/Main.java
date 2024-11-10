dfa55ab35f0c5b4eca129c06c7b24e639aa007e3
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2024-11-10 17:27:55 +0100

    :zap: change getyear logic

--- src/Main.java
+++ src/Main.java
@@ -7,25 +7,44 @@ public class Main {
     private static Integer getYear(Scanner scanner) {
         int currentYear = Calendar.getInstance().get(Calendar.YEAR);
         int minYear = currentYear - 120;
+        boolean retry;
 
-        System.out.print("Podaj swój czterocyfrowy rok urodzenia: ");
+        do {
+            System.out.print("Podaj czterocyfrowy rok urodzenia, np. 1993: ");
+            retry = false;
 
-        try {
-            Integer dateOfBirth = scanner.nextInt();
+            try {
+                Integer dateOfBirth = scanner.nextInt();
 
-            if (minYear > dateOfBirth) {
-                throw new IllegalArgumentException("Błąd: Rok urodzenia jest za mały. Minimalny rok urodzenia to: " + minYear + ".");
+                if (minYear > dateOfBirth) {
+                    throw new IllegalArgumentException("Błąd: Rok urodzenia jest za mały. Minimalny rok urodzenia to: " + minYear + ".");
+                }
+
+                if (dateOfBirth > currentYear) {
+                    throw new IllegalArgumentException("Błąd: Rok urodzenia jest za duży. Nie możesz się urodzić w przyszłości.");
+                }
+
+                return dateOfBirth;
+
+            } catch (InputMismatchException e) {
+                scanner.next(); // Clear invalid input
+                System.out.println("Błąd: Wprowadź liczbę całkowitą.");
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
 
-            if (dateOfBirth > currentYear) {
-                throw new IllegalArgumentException("Błąd: Rok urodzenia jest za duży. Nie możesz się urodzić w przyszłości.");
+            System.out.print("Podałeś złą formę roku, wciśnij 'p' jeśli chcesz podać ponownie rok lub wciśnij inny klawisz jeśli chcesz zakończyć: ");
+            String choice = scanner.next();
+
+            if (choice.equalsIgnoreCase("p")) {
+                retry = true;
             }
 
-            return dateOfBirth;
-        } catch (InputMismatchException e) {
-            scanner.next(); // Oczyszczenie błędnego wejścia
-            throw new IllegalArgumentException("Błąd: Wprowadź liczbę całkowitą.");
-        }
+        } while (retry);
+
+        System.out.println("Zakończono wprowadzanie roku.");
+        return null;
     }
 
     public static void main(String[] args) {
