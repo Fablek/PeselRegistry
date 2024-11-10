278f29b783b87b3ec0745346fdfcff4d3f6105d0
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2024-11-10 15:41:19 +0100

    :sparkles: getYear method

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,39 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Calendar;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
+public class Main {
+
+    private static Integer getYear(Scanner scanner) {
+        int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+        int minYear = currentYear - 120;
+
+        System.out.print("Podaj swój czterocyfrowy rok urodzenia: ");
+
+        try {
+            Integer dateOfBirth = scanner.nextInt();
+
+            if (minYear > dateOfBirth) {
+                throw new IllegalArgumentException("Błąd: Rok urodzenia jest za mały. Minimalny rok urodzenia to: " + minYear + ".");
+            }
+
+            if (dateOfBirth > currentYear) {
+                throw new IllegalArgumentException("Błąd: Rok urodzenia jest za duży. Nie możesz się urodzić w przyszłości.");
+            }
+
+            return dateOfBirth;
+        } catch (InputMismatchException e) {
+            scanner.next(); // Oczyszczenie błędnego wejścia
+            throw new IllegalArgumentException("Błąd: Wprowadź liczbę całkowitą.");
+        }
     }
+
+    public static void main(String[] args) {
+        System.out.println("Zadanie 1 - Pesel");
+
+        Scanner scanner = new Scanner(System.in);
+
+        getYear(scanner);
+    }
+
 }
