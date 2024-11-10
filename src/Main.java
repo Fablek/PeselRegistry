df5028721aa47b4b6f4bd2650db317d0a42862c1
Sebastian Pytka <76405637+Fablek@users.noreply.github.com>
2024-11-10 17:37:06 +0100

    :sparkles: getMonth and parseMonth functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.Calendar;
 import java.util.InputMismatchException;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -47,12 +48,81 @@ public class Main {
         return null;
     }
 
+    private static Integer getMonth(Scanner scanner) {
+        boolean retry;
+
+        do {
+            System.out.print("Podaj miesiąc urodzenia, np. czerwiec albo 6: ");
+            retry = false;
+
+            String monthInput = scanner.next().toLowerCase(Locale.ROOT);
+
+            try {
+                Integer month = parseMonth(monthInput);
+
+                if (month == null || month < 1 || month > 12) {
+                    throw new IllegalArgumentException("Błąd: Podano nieprawidłowy miesiąc.");
+                }
+
+                return month;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+
+            System.out.print("Podałeś złą formę miesiąca, wciśnij 'p' jeśli chcesz podać ponownie miesiąc lub wciśnij inny klawisz jeśli chcesz zakończyć: ");
+            String choice = scanner.next();
+
+            if (choice.equalsIgnoreCase("p")) {
+                retry = true;
+            }
+
+        } while(retry);
+
+        return null;
+    }
+
+    private static Integer parseMonth(String monthInput) {
+        try {
+            return Integer.parseInt(monthInput);
+        } catch (NumberFormatException e) {
+            switch (monthInput) {
+                case "styczeń":
+                    return 1;
+                case "luty":
+                    return 2;
+                case "marzec":
+                    return 3;
+                case "kwiecień":
+                    return 4;
+                case "maj":
+                    return 5;
+                case "czerwiec":
+                    return 6;
+                case "lipiec":
+                    return 7;
+                case "sierpień":
+                    return 8;
+                case "wrzesień":
+                    return 9;
+                case "październik":
+                    return 10;
+                case "listopad":
+                    return 11;
+                case "grudzień":
+                    return 12;
+                default:
+                    return null; // Invalid month name
+            }
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Zadanie 1 - Pesel");
 
         Scanner scanner = new Scanner(System.in);
 
         getYear(scanner);
+        getMonth(scanner);
     }
 
 }
